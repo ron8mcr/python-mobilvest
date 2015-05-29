531338c871d9443f2fefa6504781a03a90dc539f
ron8mcr <rons_mail@mail.ru>
2015-05-29 20:07:45 +0700

    Fix bug with installation

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 from setuptools import setup
-from mobilvest import VERSION
 
 f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
 readme = f.read()
@@ -8,7 +7,7 @@ f.close()
 
 setup(
     name='python-mobilvest',
-    version=".".join(map(str, VERSION)),
+    version="0.0.1",
     description='Application for working with API of mobilvest.ru',
     long_description=readme,
     author="Roman Gorbil",
